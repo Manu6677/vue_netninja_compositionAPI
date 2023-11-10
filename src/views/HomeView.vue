9c8abf3c2ecded110e8168d2f5341a5162f6a2f0
manupanjoria <manu.panjoria@bonamisoftware.com>
2023-11-10 12:18:18 +0530

    composition-api-1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <input type="text" placeholder="search Here" v-model="search" />
+    <p>Searched term: {{ search }}</p>
+    <div v-for="name in searchTerm" :key="name">{{ name }}</div>
+    <button @click="handleClick">Stop watching</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref, computed, watch, watchEffect } from "vue";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  setup() {
+    const search = ref("");
+    const names = ref([
+      "mario",
+      "yoshi",
+      "luigi",
+      "toad",
+      "bowser",
+      "koopa",
+      "peach",
+    ]);
+
+    const stopWatch = watch(search, () => {
+      console.log("watch ran");
+    });
+
+    const stopEffect = watchEffect(() => {
+      console.log("i am watch effect", search.value);
+    });
+
+    const searchTerm = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+
+    const handleClick = () => {
+      stopWatch();
+      stopEffect();
+    };
+    return { names, search, searchTerm, handleClick };
+  },
+};
+
+/**
+  -> Remember reactive does not work on the Primitive values
+
+  -> Computed properties are used to calculate the value of a property based on some other conditions.
+
+  -> Watchers, on the other hand, are not primarily used for changing the value of a property; 
+      ->instead, they are used to notify you when the value has changed and let you perform certain actions based on these changes.
+
+  -> Computed properties should be used when you need to get the current value of an object and use it in your logic, such as calculating something based on it.
+  -> And watchers should be used when you need to know when one or more values have changed and react accordingly.
+
+  -> watchEffect: it runs initially at first and when we are watching something it runs again and again
+
+  -> Remember you can STOP "Watch" and "watchEffect" just by invoking the function
+     -> In stopWatch and stopEffect we are getting the function and in handle click we are invoking them to stop
+
+
+*/
 </script>
